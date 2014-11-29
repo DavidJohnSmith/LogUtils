dbc7b7602d4cc862ff279318eb95efdacdb8db6c
DavidJohnSmith <DavidJohnSmith@users.noreply.github.com>
2014-11-29 23:01:28 +0800

    Create LogUtils.java

---
+++ LogUtils.java
@@ -0,0 +1,131 @@
+
+import android.util.Log;
+
+public class LogUtils {
+    private LogUtils() {
+    }
+
+    public static boolean allowD = true;
+    public static boolean allowE = true;
+    public static boolean allowI = true;
+    public static boolean allowV = true;
+    public static boolean allowW = true;
+    public static boolean allowWtf = true;
+
+    private static String generateTag(StackTraceElement caller) {
+        return caller.toString();
+    }
+
+
+    public static void d(String content) {
+        if (!allowD) return;
+        StackTraceElement caller = getCallerStackTraceElement();
+        String tag = generateTag(caller);
+        Log.d(tag, content);
+    }
+
+    public static void d(String content, Throwable tr) {
+        if (!allowD) return;
+        StackTraceElement caller = getCallerStackTraceElement();
+        String tag = generateTag(caller);
+        Log.d(tag, content, tr);
+    }
+
+    public static void e(String content) {
+        if (!allowE) return;
+        StackTraceElement caller = getCallerStackTraceElement();
+        String tag = generateTag(caller);
+        Log.e(tag, content);
+    }
+
+    public static void e(String content, Throwable tr) {
+        if (!allowE) return;
+        StackTraceElement caller = getCallerStackTraceElement();
+        String tag = generateTag(caller);
+        Log.e(tag, content, tr);
+    }
+
+    public static void i(String content) {
+        if (!allowI) return;
+        StackTraceElement caller = getCallerStackTraceElement();
+        String tag = generateTag(caller);
+        Log.i(tag, content);
+    }
+
+    public static void i(String content, Throwable tr) {
+        if (!allowI) return;
+        StackTraceElement caller = getCallerStackTraceElement();
+        String tag = generateTag(caller);
+        Log.i(tag, content, tr);
+    }
+
+    public static void i(int content) {
+        if (!allowI) return;
+        StackTraceElement caller = getCallerStackTraceElement();
+        String tag = generateTag(caller);
+        Log.i(tag, String.valueOf(content));
+    }
+
+    public static void v(String content) {
+        if (!allowV) return;
+        StackTraceElement caller = getCallerStackTraceElement();
+        String tag = generateTag(caller);
+        Log.v(tag, content);
+    }
+
+    private static StackTraceElement getCallerStackTraceElement() {
+        return Thread.currentThread().getStackTrace()[4];
+    }
+
+
+    public static void v(String content, Throwable tr) {
+        if (!allowV) return;
+        StackTraceElement caller = getCallerStackTraceElement();
+        String tag = generateTag(caller);
+        Log.v(tag, content, tr);
+    }
+
+    public static void w(String content) {
+        if (!allowW) return;
+        StackTraceElement caller = getCallerStackTraceElement();
+        String tag = generateTag(caller);
+        Log.w(tag, content);
+    }
+
+    public static void w(String content, Throwable tr) {
+        if (!allowW) return;
+        StackTraceElement caller = getCallerStackTraceElement();
+        String tag = generateTag(caller);
+        Log.w(tag, content, tr);
+    }
+
+    public static void w(Throwable tr) {
+        if (!allowW) return;
+        StackTraceElement caller = getCallerStackTraceElement();
+        String tag = generateTag(caller);
+        Log.w(tag, tr);
+    }
+
+
+    public static void wtf(String content) {
+        if (!allowWtf) return;
+        StackTraceElement caller = getCallerStackTraceElement();
+        String tag = generateTag(caller);
+        Log.wtf(tag, content);
+    }
+
+    public static void wtf(String content, Throwable tr) {
+        if (!allowWtf) return;
+        StackTraceElement caller = getCallerStackTraceElement();
+        String tag = generateTag(caller);
+        Log.wtf(tag, content, tr);
+    }
+
+    public static void wtf(Throwable tr) {
+        if (!allowWtf) return;
+        StackTraceElement caller = getCallerStackTraceElement();
+        String tag = generateTag(caller);
+        Log.wtf(tag, tr);
+    }
+
+}
